821fe097bd3db2485e6b936322a271aa58bbc773
dani <drodri13@hotmail.com>
2023-11-01 00:01:05 +0000

    testing Pycharm connection adding a line of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-streamlit("this is a test verification")
+streamlit("this is a test verification test")
 
 streamlit.dataframe(my_fruit_list)
