b496de6adb9e0e99d068b28f7c5ded8531c9f215
dani <drodri13@hotmail.com>
2023-11-04 19:25:04 +0000

    Show only the selected fruits on the grid

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit. header ('Fruityvice Fruit Advice!')
+#User to select the fruit he wants to see information
+fruit_choice = streamlit.text_input("What fruit would you like information about?","Kiwi")
+streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
