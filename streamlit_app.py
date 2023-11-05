8cb542707518ef5ae6a995d8eb14d94f85341e78
dani <drodri13@hotmail.com>
2023-11-05 22:56:14 +0000

    Show only the selected fruits on the grid

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas
-import requests
+#import pandas
+#import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Parents New Healthy Dinner")
 streamlit.header("Breakfast Menu")
@@ -21,6 +22,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit. header ('Fruityvice Fruit Advice!')
+
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?")
+if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas. json_normalize(fruityvice_response.json ())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error())
 #User to select the fruit he wants to see information
 fruit_choice = streamlit.text_input("What fruit would you like information about?","Kiwi")
 streamlit.write('The user entered', fruit_choice)
@@ -31,6 +43,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it as a table
 streamlit.dataframe(fruityvice_normalized)
+
+#Don't run anything until we solve the streamlit issues of adding values
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
